e03a30601e5bf756fc3f25b90b1b32ff859178a1
JrodManU <jrodmanu@yahoo.com>
2017-06-22 18:59:09 -0400

    added comments and more validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments
   belongs_to :user
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 2000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   has_secure_password
-  validates :username, presence: true, length: { minimum: 6 }, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :username, presence: true, length: { minimum: 6, maximum: 20 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 50 }
 end
